35599805fc7d853c88967d206d77561cdc0a1696
Ares <aresnemeia@gmail.com>
2025-10-20 22:24:22 -0300

    Adicionados elementos na página inicial

--- src/app/features/index/index.component.ts
+++ src/app/features/index/index.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   templateUrl: './index.component.html'
 })
 export class IndexComponent implements OnInit, OnDestroy {
-  private currentSlide = 1;
+  currentSlide = 1;
   private autoplayInterval: any;
+  private touchStartX = 0;
+  private touchEndX = 0;
 
   constructor() {}
 
@@ -19,10 +21,57 @@ export class IndexComponent implements OnInit, OnDestroy {
     this.stopAutoplay();
   }
 
+  nextSlide(): void {
+    this.currentSlide = this.currentSlide === 3 ? 1 : this.currentSlide + 1;
+    this.resetAutoplay();
+  }
+
+  previousSlide(): void {
+    this.currentSlide = this.currentSlide === 1 ? 3 : this.currentSlide - 1;
+    this.resetAutoplay();
+  }
+
+  onTouchStart(event: TouchEvent): void {
+    this.touchStartX = event.changedTouches[0].screenX;
+  }
+
+  onTouchEnd(event: TouchEvent): void {
+    this.touchEndX = event.changedTouches[0].screenX;
+    this.handleSwipe();
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    this.touchStartX = event.screenX;
+  }
+
+  onMouseUp(event: MouseEvent): void {
+    this.touchEndX = event.screenX;
+    this.handleSwipe();
+  }
+
+  private handleSwipe(): void {
+    const swipeThreshold = 50;
+    const diff = this.touchStartX - this.touchEndX;
+
+    if (Math.abs(diff) > swipeThreshold) {
+      if (diff > 0) {
+        // Swipe para esquerda - próximo slide
+        this.nextSlide();
+      } else {
+        // Swipe para direita - slide anterior
+        this.previousSlide();
+      }
+    }
+  }
+
+  private resetAutoplay(): void {
+    this.stopAutoplay();
+    this.startAutoplay();
+  }
+
   private startAutoplay(): void {
     this.autoplayInterval = setInterval(() => {
       this.currentSlide = this.currentSlide === 3 ? 1 : this.currentSlide + 1;
-      window.location.hash = `slide${this.currentSlide}`;
     }, 3000); // Troca a cada 3 segundos
   }
 
