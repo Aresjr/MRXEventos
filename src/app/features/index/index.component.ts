cae2013d106329265e7f90d18bc90841a0a2122b
Ares <aresnemeia@gmail.com>
2025-10-20 21:34:41 -0300

    Adicionados elementos na página inciial

--- src/app/features/index/index.component.ts
+++ src/app/features/index/index.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   imports: [],
   templateUrl: './index.component.html'
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit, OnDestroy {
+  private currentSlide = 1;
+  private autoplayInterval: any;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.startAutoplay();
+  }
+
+  ngOnDestroy(): void {
+    this.stopAutoplay();
+  }
+
+  private startAutoplay(): void {
+    this.autoplayInterval = setInterval(() => {
+      this.currentSlide = this.currentSlide === 3 ? 1 : this.currentSlide + 1;
+      window.location.hash = `slide${this.currentSlide}`;
+    }, 3000); // Troca a cada 3 segundos
+  }
+
+  private stopAutoplay(): void {
+    if (this.autoplayInterval) {
+      clearInterval(this.autoplayInterval);
+    }
+  }
 }
